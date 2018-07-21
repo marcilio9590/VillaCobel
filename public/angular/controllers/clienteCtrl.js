46e0993d923409f57f10274fa887c71eb3cf7b38
marcilio9590 <marcilio9590@github.com>
2018-07-21 17:24:21 -0300

    ajustes campos telefone

--- public/angular/controllers/clienteCtrl.js
+++ public/angular/controllers/clienteCtrl.js
@@ -6,14 +6,19 @@ myApp.controller("clientesCtrl", function ($scope, $http, clientesFactory, clien
 
     vm.limpar = function () {
         vm.cliente = {};
+        vm.cliente.telefones = [];
     }
 
     vm.salvar = function () {
 
     }
 
-    vm.adicionarTelefone = function(){
-        
+    vm.adicionarTelefone = function () {
+        vm.cliente.telefones.push({ id: 'telefone' + vm.cliente.telefones.length, valor: "" });
+    }
+
+    vm.removerTelefone = function (i) {
+        vm.cliente.telefones.splice(i, 1);
     }
 
     function obterClientes() {
@@ -21,13 +26,13 @@ myApp.controller("clientesCtrl", function ($scope, $http, clientesFactory, clien
     }
 
     function mock() {
-        vm.clientes.push({ idCliente: 1, nome: "Cliente 1", endereco: "Rua 1", telefones:[{ ddd:"81", numero: "9999999999" }] });
-        vm.clientes.push({ idCliente: 2, nome: "Cliente 2", endereco: "Rua 2", telefones:[{ ddd:"81", numero: "9999999999" }] });
-        vm.clientes.push({ idCliente: 3, nome: "Cliente 3", endereco: "Rua 3", telefones:[{ ddd:"81", numero: "9999999999" }] });
-        vm.clientes.push({ idCliente: 4, nome: "Cliente 4", endereco: "Rua 4", telefones:[{ ddd:"81", numero: "9999999999" }] });
-        vm.clientes.push({ idCliente: 5, nome: "Cliente 5", endereco: "Rua 5", telefones:[{ ddd:"81", numero: "9999999999" }] });
-        vm.clientes.push({ idCliente: 6, nome: "Cliente 6", endereco: "Rua 6", telefones:[{ ddd:"81", numero: "9999999999" }, { ddd:"81", numero: "9999999999" }] });
-        vm.clientes.push({ idCliente: 7, nome: "Cliente 7", endereco: "Rua 6", telefones:[{ ddd:"81", numero: "9999999999" }, { ddd:"81", numero: "9999999999" }] });
+        vm.clientes.push({ idCliente: 1, nome: "Cliente 1", endereco: "Rua 1", telefones: [{ ddd: "81", numero: "9999999999" }] });
+        vm.clientes.push({ idCliente: 2, nome: "Cliente 2", endereco: "Rua 2", telefones: [{ ddd: "81", numero: "9999999999" }] });
+        vm.clientes.push({ idCliente: 3, nome: "Cliente 3", endereco: "Rua 3", telefones: [{ ddd: "81", numero: "9999999999" }] });
+        vm.clientes.push({ idCliente: 4, nome: "Cliente 4", endereco: "Rua 4", telefones: [{ ddd: "81", numero: "9999999999" }] });
+        vm.clientes.push({ idCliente: 5, nome: "Cliente 5", endereco: "Rua 5", telefones: [{ ddd: "81", numero: "9999999999" }] });
+        vm.clientes.push({ idCliente: 6, nome: "Cliente 6", endereco: "Rua 6", telefones: [{ ddd: "81", numero: "9999999999" }, { ddd: "81", numero: "9999999999" }] });
+        vm.clientes.push({ idCliente: 7, nome: "Cliente 7", endereco: "Rua 6", telefones: [{ ddd: "81", numero: "9999999999" }, { ddd: "81", numero: "9999999999" }] });
     }
 
     function activate() {
