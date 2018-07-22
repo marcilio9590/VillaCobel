a05cca316600b33f44f203178e72e5dbae99147c
marcilio9590 <marcilio9590@github.com>
2018-07-21 21:07:54 -0300

    ajustes

--- public/angular/services/modalService.js
+++ public/angular/services/modalService.js
@@ -3,20 +3,21 @@ myApp.factory('modalFactory', function () {
     function open(id, titulo, showButtons, data) {
         $("#" + id).modal();
         var modal = $("#" + id);
+        popularModal(modal, titulo, data);
+    }
+
+    function popularModal(modal, titulo, data) {
         modal.find('.modal-title').text(titulo);
         modal.find('.nome').text(data.nome);
         modal.find('.endereco').text(data.endereco);
         var telefones = modal.find('.telefones');
         var aux = "";
-
         for (let i = 0; i < data.telefones.length; i++) {
             var key = data.telefones[i];
             aux += "(" + key.ddd + ") " + key.numero;
-            aux += i == data.telefones.length - 1 ? "" : " - "
+            aux += i == data.telefones.length - 1 ? "" : " - ";
         }
-
         telefones.text(aux);
-
     }
 
     var exports = {
@@ -26,3 +27,4 @@ myApp.factory('modalFactory', function () {
     return exports;
 
 });
+
